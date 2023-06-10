95269b41e4f3a69956b50c4ca28fbd7fe12ea808
Access42code <80382104+Access42code@users.noreply.github.com>
2023-06-11 00:07:59 +0300

    Add files via upload

---
+++ reset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    private bool isPaused = false; 
+
+    
+    public void OnPauseButtonClicked()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    
+    private void PauseGame()
+    {
+        Time.timeScale = 0f; 
+        isPaused = true;
+    }
+
+    
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f; 
+        isPaused = false;
+    }
+}
---
+++ task3.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class ToggleContainer : MonoBehaviour
+{
+    public GameObject container; 
+
+    private bool isContainerVisible = false; 
+    private bool isPaused = false; 
+    
+    public void OnMenuButtonClicked()
+    {
+        if (isContainerVisible)
+        {
+            HideContainer();
+        }
+        else
+        {
+            ShowContainer();
+        }
+        TogglePause();
+    }
+
+    
+    private void ShowContainer()
+    {
+        container.SetActive(true);
+        isContainerVisible = true;
+    }
+
+    
+    private void HideContainer()
+    {
+        container.SetActive(false);
+        isContainerVisible = false;
+    }
+
+    
+    private void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    
+    private void PauseGame()
+    {
+        Time.timeScale = 0f; 
+        isPaused = true;
+    }
+
+    
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f; 
+        isPaused = false;
+    }
+}
